ba78aaf735531f924eee140b09f200b34b969549
Nate Fuller <natefuller8@gmail.com>
2026-04-08 12:18:32 -0400

    Added mesh.cpp with notes about our proposed implementation for uploading mesh data to VRAM.

---
+++ src/mesh.cpp
@@ -0,0 +1,10 @@
+#include "headers.h"
+
+void LoadMeshes(State *state)
+{
+    // this is where we will load all the mesh data specified into vram
+    // unless we have truly egregious meshes we can probably keep them
+    // for the entire application
+
+    // textures will likely have to be streamed though
+}
